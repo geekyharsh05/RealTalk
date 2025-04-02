eb264d9d4018592cab456365f278861c6e602bca
Harsh <hp424805@gmail.com>
2025-04-02 21:19:51 +0530

    chore: init project

--- apps/frontend/src/app/index.tsx
+++ apps/frontend/src/app/index.tsx
@@ -7,19 +7,10 @@ function App() {
     <div className="container">
       <h1 className="title">
         Admin <br />
-        <span>Kitchen Sink</span>
+        <span>Real Talk</span>
       </h1>
       <CounterButton />
-      <p className="description">
-        Built With{" "}
-        <Link href="https://turbo.build/repo" newTab>
-          Turborepo
-        </Link>
-        {" & "}
-        <Link href="https://vitejs.dev/" newTab>
-          Vite
-        </Link>
-      </p>
+      <p className="description"></p>
     </div>
   );
 }

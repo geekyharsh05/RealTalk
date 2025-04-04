836837a4090f3230e0c530a22c1188f3a4b84554
Harsh <hp424805@gmail.com>
2025-04-04 20:42:42 +0530

    feat(dependencies): add ws package and its types for WebSocket support; implement MessageService for handling user messages

---
+++ apps/backend/src/services/message.service.ts
@@ -0,0 +1,31 @@
+import User from "../models/user.model";
+import Message from "../models/message.model";
+import { uploadImageToCloudinary } from "../lib/cloudinary";
+
+export class MessageService {
+    public async fetchUsersForSidebar(loggedInUserId: string) {
+        return await User.find({ _id: { $ne: loggedInUserId } }).select("-password");
+    }
+
+    public async fetchMessagesBetweenUsers(myId: string, userToChatId: string) {
+        return await Message.find({
+            $or: [
+              { senderId: myId, receiverId: userToChatId },
+              { senderId: userToChatId, receiverId: myId },
+            ],
+          });
+    }
+
+    public async createMessage(senderId: string, receiverId: string, text: string, image: string) {
+      const imageUrl = image ? await uploadImageToCloudinary(image) : undefined;
+
+      const newMessage = new Message({
+        senderId,
+        receiverId,
+        text,
+        image: imageUrl,
+      });
+  
+      return await newMessage.save();
+    }
+}

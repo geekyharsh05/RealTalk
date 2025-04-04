23a3af83635e36ebe88b59e8e8c697bc4ef3ee0e
Harsh <hp424805@gmail.com>
2025-04-05 00:23:30 +0530

    chore(dependencies): update pnpm-lock.yaml with new package versions and add new dependencies for frontend and backend; refactor environment variable for Cloudinary

--- packages/validators/src/index.ts
+++ packages/validators/src/index.ts
@@ -1,7 +1,7 @@
 import { z, ZodType } from 'zod';
-import { signUpSchema, signInSchema } from './schemas/user.schema';
+import { signUpSchema, signInSchema } from './schema/user.schema';
 
-export * from "./schemas/user.schema";
+export * from "./schema/user.schema";
 export type SignUpInput = z.infer<typeof signUpSchema>;
 export type SignInInput = z.infer<typeof signInSchema>;
 

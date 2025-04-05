c6819d19bbae4d84fbeecfb0e58d3ea420d25865
Harsh <hp424805@gmail.com>
2025-04-06 04:28:16 +0530

    Update cookie settings in token generation: change SameSite attribute from "none" to "strict" for enhanced security in generateToken.util.ts.

--- backend/src/utils/generateToken.util.ts
+++ backend/src/utils/generateToken.util.ts
@@ -9,7 +9,7 @@ export const generateToken = (userId: string, res: Response) => {
   res.cookie("jwt", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000, // MS
     httpOnly: true,
-    sameSite: "none", 
+    sameSite: "strict", 
     secure: true,
   });
 
